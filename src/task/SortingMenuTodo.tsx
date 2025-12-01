65910532816c5e3223b7ca37261e82669523525e
thomaslucking <telucking@gmail.com>
2025-12-01 16:15:42 +0100

    chore: formatting

--- src/task/SortingMenuTodo.tsx
+++ src/task/SortingMenuTodo.tsx
@@ -1,22 +1,22 @@
 export default function SortingMenuTodo() {
   return (
-      <div className="Sorting-menu-div">
-        <select className="Sorting-menu">
+    <div className="Sorting-menu-div">
+      <select className="Sorting-menu">
         <option className="Value" value="sort-by-name">
-            Name
-          </option>
-          <option className="Value" value="option2">
-            Due Date
-          </option>
-        </select>
-        <select className="Sorting-menu">
-          <option className="Value" value="option1">
-            done
-          </option>
-          <option className="Value" value="option1">
-            Undone
-          </option>
-        </select>
-      </div>
+          Name
+        </option>
+        <option className="Value" value="option2">
+          Due Date
+        </option>
+      </select>
+      <select className="Sorting-menu">
+        <option className="Value" value="option1">
+          done
+        </option>
+        <option className="Value" value="option1">
+          Undone
+        </option>
+      </select>
+    </div>
   );
 }
